39cf24311b21d817405ebe7af6027b70b3f7257b
shenperson <wayne19990609@gmail.com>
2019-09-24 02:41:57 +0800

    HW1 finish

--- HWs/PA1/src/sort_tool.cpp
+++ HWs/PA1/src/sort_tool.cpp
@@ -36,12 +36,26 @@ void SortTool::QuickSortSubVector(vector<int> &data, int low, int high) {
   // TODO : Please complete QuickSortSubVector code here
   // Hint : recursively call itself
   //        Partition function is needed
+  if (low < high) {
+    int mid = Partition(data, low, high);
+    QuickSortSubVector(data, low, mid - 1);
+    QuickSortSubVector(data, mid + 1, high);
+  }
 }
 
 int SortTool::Partition(vector<int> &data, int low, int high) {
   // Function : Partition the vector
   // TODO : Please complete the function
   // Hint : Textbook page 171
+  int pivot = data[high], i = low - 1;
+  for (int j = low; j <= high, j != high; ++j) {
+    if (data[j] <= pivot) {
+      ++i;
+      swap(data[i], data[j]);
+    }
+  }
+  swap(data[i + 1], data[high]);
+  return i + 1;
 }
 
 // Merge sort method
@@ -57,6 +71,7 @@ void SortTool::MergeSortSubVector(vector<int> &data, int low, int high) {
   //        Merge function is needed
   if (low < high) {
     int middle = (low + high) / 2;
+
     MergeSortSubVector(data, low, middle);
     MergeSortSubVector(data, middle + 1, high);
     Merge(data, low, middle, middle + 1, high);
@@ -70,37 +85,30 @@ void SortTool::Merge(vector<int> &data, int low, int middle1, int middle2,
   // TODO : Please complete the function
   int n1 = middle1 - low + 1;
   int n2 = high - middle2 + 1;
-  cout << low << ' ' << high << ' ' << middle1 << "\n";
-  vector<int> L(data.begin() + low, data.begin() + middle1),
-      R(data.begin() + middle2, data.begin() + middle1 );
-  //   L.assign(data.begin() + low, data.begin() + middle1 + 1);
-  for (auto &i : L) {
-    cout << i << '\n';
-  }
+  int L[n1], R[n2];
+  for (int i = 0; i < n1; ++i) L[i] = data[low + i];
 
-  //   R.assign(data.begin() + middle2, data.begin() + middle1 + 1);
-  for (auto &i : R) {
-    cout << i << '\n';
-  }
+  for (int i = 0; i < n2; ++i) R[i] = data[middle2 + i];
+
   int i = 0, j = 0;
-  for (int k = low; k < high; ++k) {
+  for (int k = low; k <= high; ++k) {
     if (i < n1 && j < n2) {
       if (L[i] <= R[j]) {
-        data[k - 1] = L[i];
+        data[k] = L[i];
         ++i;
       } else {
-        data[k - 1] = R[j];
+        data[k] = R[j];
         ++j;
       }
     } else {
       if (i >= n1) {
         for (int m = j; m < n2; ++m) {
-          data[m - 1] = R[j];
+          data[low + i + m] = R[j];
           ++j;
         }
       } else
-        for (int m = i; m < n2; ++m) {
-          data[m - 1] = L[i];
+        for (int m = i; m < n1; ++m) {
+          data[low + j + m] = L[i];
           ++i;
         }
       break;
@@ -127,6 +135,16 @@ void SortTool::MaxHeapify(vector<int> &data, int root) {
   // Function : Make tree with given root be a max-heap if both right and left
   // sub-tree are max-heap
   // TODO : Please complete max-heapify code here
+
+  int largest = root;
+  if (2 * root + 1 < heapSize && data[2 * root + 1] > data[largest])
+    largest = 2 * root + 1;
+  if (2 * root + 2 < heapSize && data[2 * root + 2] > data[largest])
+    largest = 2 * root + 2;
+  if (largest != root) {
+    swap(data[root], data[largest]);
+    MaxHeapify(data, largest);
+  }
 }
 
 // Build max heap
@@ -134,4 +152,7 @@ void SortTool::BuildMaxHeap(vector<int> &data) {
   heapSize = data.size();  // initialize heap size
                            // Function : Make input data become a max-heap
                            // TODO : Please complete BuildMaxHeap code here
+  for (int i = heapSize / 2; i >= 0; --i) {
+    MaxHeapify(data, i);
+  }
 }
