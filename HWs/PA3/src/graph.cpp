345f114340dfbdbb0d5e13f805778aa4ed4fee66
shenperson <wayne19990609@gmail.com>
2019-12-20 01:51:05 +0800

    new prim

--- HWs/PA3/src/graph.cpp
+++ HWs/PA3/src/graph.cpp
@@ -1,9 +1,9 @@
 #include "graph.h"
+#include <bits/stdc++.h>
 #include <algorithm>
 #include <fstream>
 #include <string>
 #include <unordered_set>
-
 #define undirected 1
 #define directed 0
 
@@ -61,33 +61,48 @@ void Graph::dump_file(char const* path) {
     ss << i->u << ' ' << i->v << ' ' << i->w << '\n';
   }
 }
-
+// ( key, id )
+typedef pair<int, int> Pair;
 /* ----- undirected ----- */
 void Graph::MST_PRIM() {
-  auto comp = [&](int a, int b) { return vertices[a].key > vertices[b].key; };
-  vector<int> Q;
+  priority_queue<Pair, vector<Pair>, greater<Pair>> Q;
   bool* Q_map = new bool[numV]();
-  Q.resize(numV, NULL);
-  for (int i = 0; i < numV; ++i) {
-    Q[i] = i;
-  }
-
-  vertices[Q[0]].key = 0;
-
+  Q.push(make_pair(0, 0));
   while (!Q.empty()) {
-    make_heap(Q.begin(), Q.end(), comp);
-    pop_heap(Q.begin(), Q.end(), comp);
-    Vertex& u = vertices[Q.back()];
-    Q.pop_back();
-    Q_map[u.id] = 1;
-
-    for (auto& v : u.Adj) {
-      if (Q_map[v] != 1 && weight[u.id][v] < vertices[v].key) {
-        vertices[v].pi = u.id;
-        vertices[v].key = weight[u.id][v];
+    int u = Q.top().second;
+    Q.pop();
+    Q_map[u] = 1;
+    for (auto& v : vertices[u].Adj) {
+      if (Q_map[v] == 0 && weight[u][v] < vertices[v].key) {
+        vertices[v].pi = u;
+        vertices[v].key = weight[u][v];
+        Q.push(make_pair(weight[u][v], v));
       }
     }
   }
+
+  // auto comp = [&](int a, int b) { return vertices[a].key > vertices[b].key;
+  // }; vector<int> Q; bool* Q_map = new bool[numV](); Q.resize(numV, NULL); for
+  // (int i = 0; i < numV; ++i) {
+  //   Q[i] = i;
+  // }
+
+  // vertices[Q[0]].key = 0;
+
+  // while (!Q.empty()) {
+  //   make_heap(Q.begin(), Q.end(), comp);
+  //   pop_heap(Q.begin(), Q.end(), comp);
+  //   Vertex& u = vertices[Q.back()];
+  //   Q.pop_back();
+  //   Q_map[u.id] = 1;
+
+  //   for (auto& v : u.Adj) {
+  //     if (Q_map[v] != 1 && weight[u.id][v] < vertices[v].key) {
+  //       vertices[v].pi = u.id;
+  //       vertices[v].key = weight[u.id][v];
+  //     }
+  //   }
+  // }
   for (int i = 0; i < numE; ++i) {
     if (vertices[edges[i].u].pi != edges[i].v &&
         vertices[edges[i].v].pi != edges[i].u) {
